853bd6842d9e2d82dcffe7b01d4bde5b9f9c63a2
tannerwh <tannerwh@gmail.com>
2016-10-11 22:14:58 -0500

    Updated testing.py

---
+++ Testing.py
@@ -0,0 +1,16 @@
+def function():
+    print("IF/FOR/FUNCTIONS are all ended by indentation")
+
+test = "Variables are made like this"
+print(test)
+
+#Print hello world
+print("Hellow World!")
+
+#If statement
+if (test == None):
+    print('Goodbye world')
+else:
+    print('This is an IF statement')
+
+#End of if
